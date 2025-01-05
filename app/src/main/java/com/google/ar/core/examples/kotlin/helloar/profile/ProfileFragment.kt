d0b3389751dde011a121bf50ad9f2628b35811ef
angkmfirefoxygal <angkmfirefoxygal@sookmyung.ac.kr>
2025-01-06 00:25:34 +0900

    2025-01-05 "KaKao API setting updated with Button Activity trouble shooting"

--- app/src/main/java/com/google/ar/core/examples/kotlin/helloar/profile/ProfileFragment.kt
+++ app/src/main/java/com/google/ar/core/examples/kotlin/helloar/profile/ProfileFragment.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.ar.core.examples.kotlin.helloar.R
+import com.mpackage.network.ApiService
+
+
 
 class ProfileFragment : Fragment() {
+
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.screen_profile, container, false)
     }
 }
